f5ccbaba164465c8e42367f7a760b1548c578eb3
Jorge Y. C. Rodriguez <1929689+coquer@users.noreply.github.com>
2024-04-24 14:02:50 +0200

    chore: improve command execution (#2)

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,10 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as fs from "node:fs";
-import { exec as childExec } from 'child_process';
 
 async function run() {
 	const jsonPath = core.getInput('list')
 	const diffBranch = core.getInput('diffBranch')
-	const currentBranch = core.getInput('currentBranch')
 
 	const content = await fs.promises.readFile(jsonPath, 'utf8')
 	const list = JSON.parse(content)
@@ -15,28 +14,25 @@ async function run() {
 		return
 	}
 
-	let diff = []
-	childExec(`git diff --name-only ${diffBranch} ${currentBranch} | cut -d / -f 1 | uniq | grep -v "\\."`, (error, stdout, stderr) => {
-		if (error) {
-			core.setFailed(`exec error: ${error}`);
-			return;
-		}
+	let diff = ''
+	const command = `git diff --name-only ${{diffBranch}} | cut -d / -f 1 | uniq | grep -v "\\."`
+	try {
+		diff = await execHelper('bash', ['-c', command])
+	} catch (error) {
+		// @ts-ignore
+		core.error(error)
+	}
 
-		let values = stdout.split('\n');
-		if (values.length > 0) {
-			// @ts-ignore
-			diff.push(values);
-		}
-	});
+	const splitString = diff.split('\n')
 
-	if (diff.length === 0) {
+	if (splitString.length === 0) {
 		core.info('No files to check')
 		return
 	}
 
 	const filteredList = list.filter(({service}: any) => {
 		// @ts-ignore
-		return diff.includes(service)
+		return splitString.includes(service)
 	})
 
 	if (filteredList.length === 0) {
@@ -47,6 +43,32 @@ async function run() {
 	const filteredListString = JSON.stringify(filteredList)
 
 	core.setOutput('filtered', filteredListString)
+}
+
+
+const execHelper = async (tool, args: string[], options: exec.ExecOptions = {}) => {
+	let stdout = ""
+	let stderr = ""
+
+	const opts = {
+		...options,
+		listeners: {
+			stdout: (data) => {
+				stdout += data.toString()
+			},
+			stderr: (data) => {
+				stderr += data.toString()
+			}
+		}
+	}
+
+	const exitCode = await exec.exec(tool, args, opts)
+	if (exitCode != 0) {
+		const errMsg = `${tool} exited with code: ${exitCode} \n ${stderr}`
+		throw Error(errMsg)
+	}
+
+	return stdout
 }
 
 run().catch(core.setFailed)
